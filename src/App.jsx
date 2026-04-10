49dec14076d7dced6c75c2d8279348fa6e376da7
WernerG98 <werner.glueck@gmx.net>
2026-04-10 18:52:28 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,12 @@ export default function GlueckEngineeringWebsite() {
       text: "Passgenaue Führung für den unteren Kühlergrill des VW T4 mit langem Vorderwagen für originale Kotflügel. Bei Nachbau-Kotflügeln sind die Laschen oft höher, die Öffnung im Halter kann einfach angepasst werden.",
     },
     {
+      name: "VW T5 Clip Verstellung Kopfstütze (4 Stück)",
+      image: "/VW_T5_Clip_Verstellung_Kopfstuetze.jpg",
+      price: "9,99 €",
+      text: "Ersatzclip für die Verstellung der Kopfstütze. Standardmäßig in Schwarz, andere Wunschfarbe bitte bei der Bestellung angeben.",
+    },
+    {
       name: "Honda XRV750 RD07 Spritzschutz Schwinge hinten",
       image: "/Honda_XRV_RD07_Spritzschutz_Schwinge_hinten.jpg",
       price: "24,99 €",
